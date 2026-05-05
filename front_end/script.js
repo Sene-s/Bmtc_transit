3e15edc1639de46b087f4b9f503b68167e28a093
Sene-s <193574008+Sene-s@users.noreply.github.com>
2026-05-05 07:16:52 +0000

    done

--- front_end/script.js
+++ front_end/script.js
@@ -1,7 +1,5 @@
 
-    const API_BASE = window.location.hostname === 'localhost'
-        ? 'http://localhost:8000'
-        : '';   
+    const API_BASE = "https://bmtc-transit.onrender.com"   ; 
     const map = L.map('map', { zoomControl: true }).setView([12.97, 77.59], 12);
     L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; OpenStreetMap contributors &copy; CARTO',
